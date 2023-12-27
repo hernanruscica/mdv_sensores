ebac29e0aadfa9135a57875367cd24d350187240
hernanruscica <cesarhernanruscica@gmail.com>
2023-12-27 13:07:26 -0300

    para probar en casa

--- app/controllers/DataloggerController.js
+++ app/controllers/DataloggerController.js
@@ -31,7 +31,7 @@ module.exports = {
 
         //aca tengo que hacer la busqueda en la BD de los datos del canal y los datos de las ultimas 24 hs. o tiempo necesario para atras.
 
-        const currentData = await DataModel.getData('guemes', '2023-12-01', '2023-12-26');
+        const currentData = await DataModel.getData('guemes', '2023-12-22', '2023-12-26');
         console.log(currentData[1]);
 
         res.render('viewChannel', {user: req.session.user, id: id, idChannel: idChannel, currentData: currentData});
